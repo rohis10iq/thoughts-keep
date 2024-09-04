2e45ee75f9e18d75bbb3bc4230bd2f697c886b7e
Rohail Iqbal <leorohi10@gmail.com>
2024-09-04 19:25:40 +0500

    Refractor firebase.ts to use environment variables

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,24 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8GoZ39khiQWqNEGCEf6VJ2pzp0VrCdNs",
-  authDomain: "thoughts-keep.firebaseapp.com",
-  projectId: "thoughts-keep",
-  storageBucket: "thoughts-keep.appspot.com",
-  messagingSenderId: "453211181645",
-  appId: "1:453211181645:web:5c4be645a08547d8c4c204",
-  measurementId: "G-NEDGFGQVD6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null; // Analytics only runs in the browser
-const db = getFirestore(app); // Initialize Firestore
-const auth = getAuth(app); // Initialize Firebase Authentication
+const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null; 
+const db = getFirestore(app); 
+const auth = getAuth(app);
 
 export { app, analytics, db, auth };
