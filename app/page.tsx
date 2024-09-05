470d2f074d42190c90404281f0f2098772bf2e86
Rohail Iqbal <leorohi10@gmail.com>
2024-09-05 22:39:31 +0500

    continued debugging

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
   const handleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-
       router.push("/note");
     } catch (error) {
       console.error("Error during sign-in:", error);
@@ -25,31 +24,26 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#f5ba13] text-white selection:text-[#f3e57c]">
-      <h1 className="text-9xl font-bold text-white text-center mb-0 gradient-text">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f5ba13] text-white selection:text-[#f3e57c] p-4">
+      <h1 className="text-5xl sm:text-7xl md:text-9xl font-bold text-white text-center mb-0 gradient-text">
         Thoughts Keep
       </h1>
-      <p className="text-4xl font-bold text-[#323030] mb-8 mt-4">
+      <p className="text-xl sm:text-2xl md:text-4xl font-bold text-[#323030] mb-8 mt-4 text-center">
         Keep your thoughts together.
       </p>
-      <div className="flex space-x-6">
+      <div className="flex space-x-4 sm:space-x-6">
         <button
-          className="button py-3 px-6 rounded-lg font-bold shadow-md text-xl"
+          className="button py-2 px-4 sm:py-3 sm:px-6 rounded-lg font-bold shadow-md text-lg sm:text-xl transition-colors duration-300 ease-in-out bg-white text-[#f5ba13] hover:bg-[#f3e57c] hover:text-[#323030]"
           onClick={handleSignIn}
         >
-          {" "}
           Sign In
         </button>
-
-        <div className="flex space-x-6">
-          <button
-            className="button py-3 px-6 rounded-lg font-bold shadow-md text-xl"
-            onClick={handleContinue}
-          >
-            {" "}
-            Continue without Signing
-          </button>
-        </div>
+        <button
+          className="button py-2 px-4 sm:py-3 sm:px-6 rounded-lg font-bold shadow-md text-lg sm:text-xl transition-colors duration-300 ease-in-out bg-white text-[#f5ba13] hover:bg-[#f3e57c] hover:text-[#323030]"
+          onClick={handleContinue}
+        >
+          Continue as Guest
+        </button>
       </div>
     </div>
   );
